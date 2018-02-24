bb64ede5e962b5aaf5d7cdfd0de6128e90149f50
Trent Stenoien <trent.stenoien@gmail.com>
2018-02-23 18:37:02 -0600

    Fixed a display bug with tables, restyled header, resized init tracker.

--- js/app.js
+++ js/app.js
@@ -563,10 +563,11 @@ function operatorClick (e) {
 const tableLoader = document.querySelector('#tables');
 	const tableSelect = tableLoader.firstElementChild.firstElementChild.nextElementSibling.nextElementSibling;
 	const tableButton = tableSelect.nextElementSibling;
+	const tableImg = tableButton.nextElementSibling.nextElementSibling;
 
 tableButton.addEventListener('click', function () {
-	if (tableSelect.value) img.src = `./images/${tableSelect.value}.jpg`;
-	else img.display = 'none';
+	if (tableSelect.value) tableImg.src = `./images/${tableSelect.value}.jpg`;
+	else tableImg.display = 'none';
 });
 
 
